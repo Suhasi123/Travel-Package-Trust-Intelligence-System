fb34fa98b0a0cbc6dca2f877b5bae8666f39f59c
Suhasi Bari <barisuhasi2005@gmail.com>
2026-02-03 00:06:37 +0530

    changed allow origins

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://travel-trust.vercel.app"],
+    allow_origins=["https://travel-package-trust-intelligence-s.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
