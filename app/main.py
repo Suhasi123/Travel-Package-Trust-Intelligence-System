3315e7fa935703bf8f552cd9d924ca95678ffa81
Suhasi Bari <barisuhasi2005@gmail.com>
2026-02-02 20:20:49 +0530

    change in middleware

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://travel-trust.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
